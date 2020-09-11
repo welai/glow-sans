d722c49d9dedc0b425a68619728af6e9bbba4957
celestialphineas <hanghang0713@hotmail.com>
2020-09-11 11:26:17 +0800

    Genne Gothic added to the font directory

--- fonts/init.sh
+++ fonts/init.sh
@@ -1,101 +1,95 @@
-# Ensure that `fonts` is the current directory
-ARG=$1
+#!/bin/bash
 
-china_mirror() {
-  re='https://raw.githubusercontent.com/[^/]*/([^/]*)/(.*)'
-  if [[ $1 =~ $re ]]; then
-    CHINA_URL="https://gitee.com/celestialphineas/${BASH_REMATCH[1]}/raw/${BASH_REMATCH[2]}"
-  fi
-  unset re
-}
-
-WGET() {
-  if [[ $ARG == '-m' ]]; then
-    china_mirror $1
-    wget -q $CHINA_URL
-  else
-    wget -q $1
-  fi
-}
-
-WGET https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SourceHanSansSC.zip &
-WGET https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SourceHanSansK.zip &
-WGET https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SourceHanSansJ.zip &
+wget https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SourceHanSansSC.zip &
+wget https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SourceHanSansK.zip &
+wget https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SourceHanSansJ.zip &
 wait
 unzip \*.zip
 
 mkdir Fira
 cd Fira
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Two.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Four.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Eight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Hair.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Thin.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Two.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Four.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Eight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Hair.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Thin.otf &
 wait
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-UltraLight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-ExtraLight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Light.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Book.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Regular.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-UltraLight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-ExtraLight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Light.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Book.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Regular.otf &
 wait
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Medium.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-SemiBold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Bold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-ExtraBold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Heavy.otf &
-wait
-
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Two.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Four.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Eight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Hair.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Thin.otf &
-wait
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-UltraLight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-ExtraLight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Light.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Regular.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Book.otf &
-wait
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Medium.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-SemiBold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Bold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-ExtraBold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Heavy.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Medium.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-SemiBold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Bold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-ExtraBold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Normal/Roman/FiraSans-Heavy.otf &
 wait
 
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Two.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Four.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Eight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Hair.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Thin.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Two.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Four.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Eight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Hair.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Thin.otf &
 wait
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-UltraLight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-ExtraLight.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Light.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Regular.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Book.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-UltraLight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-ExtraLight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Light.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Regular.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Book.otf &
 wait
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Medium.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-SemiBold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Bold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-ExtraBold.otf &
-WGET https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Heavy.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Medium.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-SemiBold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Bold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-ExtraBold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Condensed/Roman/FiraSansCondensed-Heavy.otf &
+wait
+
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Two.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Four.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Eight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Hair.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Thin.otf &
+wait
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-UltraLight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-ExtraLight.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Light.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Regular.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Book.otf &
+wait
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Medium.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-SemiBold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Bold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-ExtraBold.otf &
+wget https://raw.githubusercontent.com/bBoxType/FiraSans/master/Fira_Sans_4_3/Fonts/Fira_Sans_OTF_4301/Compressed/Roman/FiraSansCompressed-Heavy.otf &
 wait
 cd ..
 
 mkdir Raleway
 cd Raleway
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Thin.otf &
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-ExtraLight.otf &
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Light.otf &
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Regular.otf &
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Medium.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Thin.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-ExtraLight.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Light.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Regular.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Medium.otf &
 wait
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-SemiBold.otf &
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Bold.otf &
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-ExtraBold.otf &
-WGET https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Black.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-SemiBold.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Bold.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-ExtraBold.otf &
+wget https://raw.githubusercontent.com/impallari/Raleway/master/fonts/v4020/Raleway-v4020-Black.otf &
+wait
+cd ..
+
+mkdir Genne
+cd Genne
+wget https://raw.githubusercontent.com/MoneMizuno/Genne-Gothic/master/GenneGothic-ExtraLight.otf &
+wget https://raw.githubusercontent.com/MoneMizuno/Genne-Gothic/master/GenneGothic-Light.otf &
+wget https://raw.githubusercontent.com/MoneMizuno/Genne-Gothic/master/GenneGothic-Normal.otf &
+wget https://raw.githubusercontent.com/MoneMizuno/Genne-Gothic/master/GenneGothic-Regular.otf &
+wget https://raw.githubusercontent.com/MoneMizuno/Genne-Gothic/master/GenneGothic-Medium.otf &
+wget https://raw.githubusercontent.com/MoneMizuno/Genne-Gothic/master/GenneGothic-Bold.otf &
+wget https://raw.githubusercontent.com/MoneMizuno/Genne-Gothic/master/GenneGothic-Heavy.otf &
 wait
 cd ..
 
